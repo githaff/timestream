bffeda20af8280965deae6ec007c3aaa98707b9c
Dmitry Lavnikevich <1469488+githaff@users.noreply.github.com>
2020-01-19 19:26:20 +0300

    Implement time stream

--- src/main.c
+++ src/main.c
@@ -1,16 +1,26 @@
 #include "aux.h"
 #include "config.h"
 
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <time.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/types.h>
 #include <time.h>
 
 
+#define SEC_PER_MIN 60
 typedef enum Mode { SECONDS, MINUTES } Mode;
 
 Mode g_mode = MINUTES;
+timer_t g_timer;
 
+int setup();
 
 void usage()
 {
@@ -47,11 +57,118 @@ void parse_args(int argc, char *argv[])
     }
 }
 
-int main(int argc, char *argv[])
+int get_time(struct timespec *ts)
 {
     int ret = 0;
 
+    ret = clock_gettime(CLOCK_REALTIME, ts);
+    if (ret < 0) {
+        perror("failed to get clock");
+        return ret;
+    }
+
+    return ret;
+}
+
+void sig_handler(int sig)
+{
+    int ret;
+    struct timespec ts;
+    struct tm tm;
+
+    if (sig == SIGUSR1) {
+        get_time(&ts);
+        ret = clock_gettime(CLOCK_REALTIME, &ts);
+        if (ret < 0) {
+            perror("failed to get clock");
+            return;
+        }
+        dbg("Latency: %ldns", ts.tv_nsec);
+
+        /* Note: it is not advised to use printf function in the signal handlers but
+         * this one is called no more frequent than once a second. */
+        localtime_r(&ts.tv_sec, &tm);
+        if (g_mode == MINUTES)
+            printf("%d:%d\n", tm.tm_hour, tm.tm_min);
+        else
+            printf("%d:%d:%d\n", tm.tm_hour, tm.tm_min, tm.tm_sec);
+    } else if (sig == SIGUSR2) {
+        g_mode = g_mode == SECONDS ? MINUTES : SECONDS;
+        setup();
+    } else {
+        err("unexpected signal: %d", sig);
+    }
+}
+
+int setup()
+{
+    int ret = 0;
+    struct timespec ts;
+    struct itimerspec its = {0};
+
+    ret = get_time(&ts);
+    if (ret < 0) {
+        err("failed to get current time");
+        return ret;
+    }
+
+    its.it_interval.tv_sec = g_mode == MINUTES ? SEC_PER_MIN : 1;
+    its.it_value.tv_sec = ts.tv_sec / SEC_PER_MIN * SEC_PER_MIN;
+
+    ret = timer_settime(g_timer, TIMER_ABSTIME, &its, NULL);
+    if (ret < 0) {
+        perror("failed to set timer");
+        return ret;
+    }
+
+    return ret;
+}
+
+int init()
+{
+    int ret = 0;
+    struct sigevent evp = {
+        .sigev_notify = SIGEV_SIGNAL,
+        .sigev_signo = SIGUSR1,
+    };
+    struct sigaction sa = {
+        .sa_handler = &sig_handler,
+        .sa_flags = 0
+    };
+    sigemptyset(&sa.sa_mask);
+
+    ret = sigaction(SIGUSR1, &sa, NULL);
+    ret &= sigaction(SIGUSR2, &sa, NULL);
+    if (ret < 0) {
+        perror("failed to set signal handler");
+        return ret;
+    }
+
+    ret = timer_create(CLOCK_REALTIME, &evp, &g_timer);
+    if (ret < 0) {
+        perror("failed to create timer");
+        return -1;
+    }
+
+    return ret;
+}
+
+int main(int argc, char *argv[]) {
+    int ret = 0;
+
     parse_args(argc, argv);
+
+    ret = init();
+    if (ret < 0)
+        return ret;
+
+    ret = setup();
+    if (ret < 0)
+        return ret;
+
+    while (true) {
+        pause();
+    }
 
     return ret;
 }
